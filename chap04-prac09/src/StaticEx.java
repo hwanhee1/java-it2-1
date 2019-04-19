92a38a918f676115846d0ab565b9e0c9eef80b6f
hwanhee1 <48575344+hwanhee1@users.noreply.github.com>
2019-04-19 11:35:25 +0900

    Update StaticEx.java

--- chap04-prac09/src/StaticEx.java
+++ chap04-prac09/src/StaticEx.java
@@ -1,17 +1,17 @@
 class ArrayUtil {	
-	public static int [] concat(int[] a, int[] b) { // 배열 a와 b를 연결한 새로운 배열 리턴
-		int tmp [] = new int [a.length + b.length]; // 배열 a와 b를 합한 크기의 배열 생성
-		for(int i=0; i<a.length; i++) // 배열 a를 tmp로 복사
+	public static int [] concat(int[] a, int[] b) {
+		int tmp [] = new int [a.length + b.length];
+		for(int i=0; i<a.length; i++)
 			tmp[i] = a[i];
 
-		for(int i=0; i<b.length; i++) { // 배열 b를 tmp로 복사
+		for(int i=0; i<b.length; i++) {
 			int index = a.length + i;
 			tmp[index] = b[i];
 		}
 
 		return tmp;
 	}
-	public static void print(int[] a) { // 배열 a 출력
+	public static void print(int[] a) {
 		System.out.print("[ ");
 		for(int i=0; i<a.length; i++) {
 			System.out.print(a[i] + " ");
